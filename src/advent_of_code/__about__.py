6199afc4dcdee47f7ca4745fb0820dcd5b1cf833
Mila Berni <mila.berni@agilelab.it>
2024-12-03 17:34:41 +0100

    move folders

--- src/advent_of_code/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Mila Berni <mila.berni@agilelab.it>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- src/advent_of_code/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Mila Berni <mila.berni@agilelab.it>
-#
-# SPDX-License-Identifier: MIT
