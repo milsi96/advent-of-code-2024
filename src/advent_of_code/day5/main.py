65d32fe119a62c67ca1f8653b783074c8af2fc1a
Mila Berni <mila.berni@agilelab.it>
2024-12-05 15:18:33 +0100

    add day 5 part two solution

--- src/advent_of_code/day5/main.py
+++ src/advent_of_code/day5/main.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from functools import partial
-from typing import List
+from functools import cmp_to_key, partial
+from typing import List, Tuple
 from advent_of_code.utils.file_utils import process_file
 
 @dataclass
@@ -9,9 +9,9 @@ class Rule:
   after: int
 
 
-def get_middle_page_sum(file_name: str) -> int:
+def _process_input(file_name: str) -> Tuple[List[Rule], List[List[int]]]:
   input: List[str] = process_file(file_name=file_name, process=lambda x: x)
-  
+
   def _process_rule(line: str) -> Rule:
     numbers = line.split("|")
     return Rule(before=int(numbers[0]), after=int(numbers[1]))
@@ -21,6 +21,11 @@ def get_middle_page_sum(file_name: str) -> int:
 
   rules: List[Rule] = list(map(_process_rule, filter(lambda line: "|" in line, input)))
   updates: List[List[int]] = list(map(_process_update, filter(lambda line: "," in line, input)))
+  return rules, updates
+
+
+def get_middle_page_sum(file_name: str) -> int:
+  rules, updates = _process_input(file_name=file_name)
   valid_updates: List[List[int]] = list(filter(partial(_is_update_valid, rules), updates))
   return sum(map(lambda update: update[int(len(update)/2)], valid_updates))
 
@@ -34,12 +39,33 @@ def _is_update_valid(rules: List[Rule], update: List[int]) -> bool:
   return True
 
 
+def get_sum_invalid_updates(file_name: str) -> int:
+  rules, updates = _process_input(file_name=file_name)
+
+  def _compare(rules: List[Rule], num1: int, num2: int) -> int:
+    applicable_rules = list(filter(lambda rule: (num1 == rule.after and num2 == rule.before) or (num1 == rule.before and num2 == rule.after), rules))
+    if len(applicable_rules) == 0:
+      return 0
+    target_rule = applicable_rules[0]
+    if num1 == target_rule.after:
+      return 1
+    else: 
+      return -1
+
+  sorted_updates = list(map(lambda u: sorted(u, key=cmp_to_key(partial(_compare, rules))), filter(lambda u: not partial(_is_update_valid, rules)(u), updates)))
+
+  return sum(map(lambda update: update[int(len(update)/2)], sorted_updates))
+
+
 
 def main() -> None:
   file_name: str = "input/day5/input.txt"
 
   part_one = get_middle_page_sum(file_name=file_name)
   print("Part one solution is:", part_one)
+
+  part_two = get_sum_invalid_updates(file_name=file_name)
+  print("Part two solution is:", part_two)
 
 
 if __name__ == "__main__":
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -1,7 +1,12 @@
 import pytest
 
-from advent_of_code.day5.main import get_middle_page_sum
+from advent_of_code.day5.main import get_sum_invalid_updates, get_middle_page_sum
 
 @pytest.mark.parametrize("file_name, expected", [("input/day5/example.txt", 143)])
 def test_middle_numbers_sum(file_name: str, expected: str) -> None:
   assert get_middle_page_sum(file_name=file_name) == expected
+
+
+@pytest.mark.parametrize("file_name, expected", [("input/day5/example.txt", 123)])
+def test_invalid_updates(file_name: str, expected: str) -> None:
+  assert get_sum_invalid_updates(file_name=file_name) == expected
