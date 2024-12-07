6662176887dcb3c04d7e7f039e46cf3c89e90454
Mila Berni <mila.berni@agilelab.it>
2024-12-07 17:30:15 +0100

    fix a bug in day 7 part two

---
+++ tests/test_day7.py
@@ -0,0 +1,7 @@
+import pytest
+
+from advent_of_code.day7.main import get_valid_equations_sum
+
+@pytest.mark.parametrize("file_name, expected, expected_with_combination", [("input/day7/example.txt", 3749, 11387)])
+def test_valid_equations(file_name: str, expected: int, expected_with_combination: int) -> None:
+  assert get_valid_equations_sum(file_name=file_name) == (expected, expected_with_combination)
