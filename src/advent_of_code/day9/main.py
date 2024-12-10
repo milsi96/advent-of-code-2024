12801020397a9a29143beff823e2725cfa7829e7
Mila Berni <mila.berni@agilelab.it>
2024-12-10 22:27:50 +0100

    temp

--- src/advent_of_code/day9/main.py
+++ src/advent_of_code/day9/main.py
@@ -1,42 +1,53 @@
+from typing import Dict, List
 from advent_of_code.utils.file_utils import process_file
 
-dot = "."
+dot = -1
 
 
-def parse_file_system(file_name: str) -> tuple[dict[str, list[int]], list[str]]:
+def parse_file_system(file_name: str) -> Dict[int, List[int]]:
     input: list[int] = process_file(
         file_name=file_name, process=lambda x: list(map(int, x.replace("\n", "")))
     )[0]
     current_file_number: int = 0
-    acc: list[str] = []
+    current_index: int = -1
+    filesystem: dict[int, list[int]] = {}
+
     for i in range(len(input)):
+        new_indexes = [(current_index := current_index + 1) for _ in range(input[i])]
         if i % 2 == 0:
-            for _ in range(input[i]):
-                acc.append(str(current_file_number))
+            filesystem[current_file_number] = new_indexes
             current_file_number += 1
         else:
-            for _ in range(input[i]):
-                acc.append(dot)
+            free_space = filesystem.get(dot, [])
+            free_space.extend(new_indexes)
+            filesystem[dot] = free_space
 
-    filesystem: dict[str, list[int]] = {}
-    for i in range(len(acc)):
-        current_indexes = filesystem.get(acc[i], [])
-        current_indexes.append(i)
-        filesystem[acc[i]] = current_indexes
-
-    return filesystem, list(filter(lambda ch: ch != dot, acc))
+    return filesystem
 
 
-def get_checksum(filesystem: dict[str, list[int]]) -> int:
+def get_checksum(filesystem: Dict[int, List[int]]) -> int:
     return sum(
-        sum(int(file) * index for index in indexes)
+        sum(file * index for index in indexes)
         for file, indexes in filesystem.items()
         if file != dot
     )
 
 
-def remove_empty_spaces(file_name: str) -> int:
-    filesystem, non_empty_spaces = parse_file_system(file_name=file_name)
+def get_non_empty_spaces(filesystem: Dict[int, List[int]]) -> List[int]:
+    return [
+        index
+        for file in [
+            [key for _ in range(len(value))]
+            for key, value in sorted(filesystem.items())
+            if key != dot
+        ]
+        for index in file
+    ]
+
+
+def fill_empty_spaces(file_name: str) -> int:
+    filesystem = parse_file_system(file_name=file_name)
+    non_empty_spaces = get_non_empty_spaces(filesystem=filesystem)
 
     for empty_space in sorted(filesystem[dot]):
         last_non_empty_item = non_empty_spaces.pop(-1)
@@ -50,9 +61,9 @@ def remove_empty_spaces(file_name: str) -> int:
     return get_checksum(filesystem=filesystem)
 
 
-def get_empty_chunks(empty_indexes: list[int]) -> list[list[int]]:
-    chunk: list[int] = []
-    result: list[list[int]] = []
+def get_empty_chunks(empty_indexes: List[int]) -> List[List[int]]:
+    chunk: List[int] = []
+    result: List[List[int]] = []
     for i in range(len(empty_indexes) - 1):
         chunk.append(empty_indexes[i])
         if empty_indexes[i + 1] - empty_indexes[i] > 1:
@@ -62,33 +73,45 @@ def get_empty_chunks(empty_indexes: list[int]) -> list[list[int]]:
 
 
 def move_files(file_name: str) -> int:
-    filesystem, _ = parse_file_system(file_name=file_name)
+    filesystem = parse_file_system(file_name=file_name)
     empty_chunks = get_empty_chunks(empty_indexes=filesystem[dot])
-    already_moved: set[str] = set()
 
-    while True:
-        empty_chunk = empty_chunks[0]
-        chunk_length = len(empty_chunk)
-        try:
-            substitute = max(
-                map(
-                    lambda entry: entry[0],
-                    filter(
-                        lambda entry: len(entry[1]) <= chunk_length
-                        and entry[0] not in already_moved,
-                        filesystem.items(),
-                    ),
-                )
+    for file in sorted(filesystem.keys(), reverse=True):
+        if file == dot:
+            continue
+
+        print(file)
+        file_length = len(filesystem[file])
+
+        def find_empty_chunk(empty_spaces: List[int], file_len: int) -> List[int]:
+            result: List[int] = [empty_spaces[0]]
+            for empty_space in empty_spaces[1:]:
+                if result[-1] - empty_space and len(result) < file_len:
+                    result.clear()
+                    continue
+                result.append(empty_space)
+
+            return result
+
+        if (
+            empty_chunk_index := find_empty_chunk(
+                empty_spaces=filesystem[dot], file_len=file_length
             )
-        except ValueError:
-            break
-        updated_indexes = empty_chunk.copy()
-        for i in range(len(filesystem[substitute])):
-            filesystem[dot].remove(empty_chunk[i])
-        filesystem[substitute] = updated_indexes[: len(filesystem[substitute])]
-        already_moved.add(substitute)
-        print(empty_chunk, substitute, len(filesystem[substitute]))
-        empty_chunks = get_empty_chunks(empty_indexes=filesystem[dot])
+        ) == []:
+            print("File", file, "couldn't be moved")
+            continue
+
+        filesystem[file].clear()
+        filesystem[file].extend(
+            (empty_chunk := empty_chunks[empty_chunk_index])[:file_length]
+        )
+        del empty_chunks[empty_chunk_index]
+        if (to_append := empty_chunk[file_length:]) != []:
+            empty_chunks.append(to_append)
+        empty_chunks = get_empty_chunks(
+            empty_indexes=sorted([index for chunk in empty_chunks for index in chunk])
+        )
+        empty_chunks.sort(key=lambda chunk: chunk[0])
 
     return get_checksum(filesystem=filesystem)
 
@@ -96,11 +119,12 @@ def move_files(file_name: str) -> int:
 def main() -> None:
     file_name: str = "input/day9/input.txt"
 
-    part_one = remove_empty_spaces(file_name=file_name)
+    part_one = fill_empty_spaces(file_name=file_name)
     print("Part one solution is:", part_one)
 
     part_two = move_files(file_name=file_name)
     print("Part two solution is:", part_two)
+    # 6636608781232
 
 
 if __name__ == "__main__":
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,11 +1,11 @@
 import pytest
 
-from advent_of_code.day9.main import move_files, remove_empty_spaces
+from advent_of_code.day9.main import move_files, fill_empty_spaces
 
 
 @pytest.mark.parametrize("file_name, expected", [("input/day9/example.txt", 1928)])
 def test_remove_empty_spaces_checksum(file_name: str, expected: int) -> None:
-    assert remove_empty_spaces(file_name=file_name) == expected
+    assert fill_empty_spaces(file_name=file_name) == expected
 
 
 @pytest.mark.parametrize("file_name, expected", [("input/day9/example.txt", 2858)])
