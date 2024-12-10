9a05cf4ea1790508691a97a8c7629a6e2ba5d173
Mila Berni <mila.berni@agilelab.it>
2024-12-10 10:11:57 +0100

    add day 9 (part 2 not working)

--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,8 +1,13 @@
 import pytest
 
-from advent_of_code.day9.main import get_checksum
+from advent_of_code.day9.main import move_files, remove_empty_spaces
 
 
 @pytest.mark.parametrize("file_name, expected", [("input/day9/example.txt", 1928)])
-def test_get_checksum(file_name: str, expected: int) -> None:
-  assert get_checksum(file_name=file_name) == expected
+def test_remove_empty_spaces_checksum(file_name: str, expected: int) -> None:
+  assert remove_empty_spaces(file_name=file_name) == expected
+
+
+@pytest.mark.parametrize("file_name, expected", [("input/day9/example.txt", 2858)])
+def test_move_files_checksum(file_name: str, expected: int) -> None:
+  assert move_files(file_name=file_name) == expected
