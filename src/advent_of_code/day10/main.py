a42498caef4d9ae4dd859cbc089f265fe6682a26
Mila Berni <mila.berni@agilelab.it>
2024-12-10 11:14:16 +0100

    add day 10 part two solution

--- src/advent_of_code/day10/main.py
+++ src/advent_of_code/day10/main.py
@@ -19,22 +19,25 @@ def next_move(input: Dict[Coordinate, int], current_point: Coordinate) -> Set[Co
   result = set(filter(lambda move: move in input.keys() and input[move] - input[current_point] == 1, new_moves))
   return result
 
-def _trigger_next_move(input: Dict[Coordinate, int], path: List[Coordinate], next_move: Coordinate) -> Set[Coordinate]:
+def _trigger_next_move(input: Dict[Coordinate, int], path: List[Coordinate], next_move: Coordinate) -> List[Coordinate]:
     new_path = path.copy()
     new_path.append(next_move)
     return get_trailheads(input=input, path=new_path)
 
-def get_trailheads(input: Dict[Coordinate, int], path: List[Coordinate]) -> Set[Coordinate]:
+def get_trailheads(input: Dict[Coordinate, int], path: List[Coordinate]) -> List[Coordinate]:
   current_point: Coordinate = path[-1]
   if input.get(current_point) == 9:
-    return {current_point}
+    return [current_point]
   
   next_moves = next_move(input=input, current_point=current_point)
   if len(next_moves) == 0:
-    return set()
+    return []
 
-  return reduce(lambda a, b: a.union(b), [_trigger_next_move(input=input, path=path, next_move=move) for move in next_moves])
+  return reduce(_reduce_list, [_trigger_next_move(input=input, path=path, next_move=move) for move in next_moves])
 
+def _reduce_list(a, b) -> List[Coordinate]:
+  a.extend(b)
+  return a
 
 def solve_part_one(file_name: str) -> int:
   lines = process_file(file_name=file_name, process=lambda line: line.replace("\n", ""))
@@ -46,7 +49,23 @@ def solve_part_one(file_name: str) -> int:
   start_points: Set[Coordinate] = set(map(lambda entry: entry[0], filter(lambda entry: entry[1] == 0, input.items())))
 
   result = {
-    start: reduce(lambda a, b: a.union(b), [_trigger_next_move(input=input, path=[], next_move=start)])
+    start: set(reduce(_reduce_list, [_trigger_next_move(input=input, path=[], next_move=start)]))
+    for start in start_points
+  }
+  
+  return len([coord for value in result.values() for coord in value])
+
+def solve_part_two(file_name: str) -> int:
+  lines = process_file(file_name=file_name, process=lambda line: line.replace("\n", ""))
+  input: Dict[Coordinate, int] = dict()
+  for row in range(len(lines)):
+    for column in range(len(lines[row])):
+      input[(row, column)] = int(lines[row][column])
+  
+  start_points: Set[Coordinate] = set(map(lambda entry: entry[0], filter(lambda entry: entry[1] == 0, input.items())))
+
+  result = {
+    start: reduce(_reduce_list, [_trigger_next_move(input=input, path=[], next_move=start)])
     for start in start_points
   }
   
@@ -57,6 +76,9 @@ def main() -> None:
 
   part_one = solve_part_one(file_name=file_name)
   print("Part one solution is:", part_one)
+
+  part_two = solve_part_two(file_name=file_name)
+  print("Part two solution is:", part_two)
 
 
 if __name__ == "__main__":
