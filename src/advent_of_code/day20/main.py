8d3f94a74e0978e7dca3e6b13f8b20de694f93ad
Mila Berni <mila.berni@agilelab.it>
2024-12-20 15:42:38 +0100

    refactor day 20 part one solution

--- src/advent_of_code/day20/main.py
+++ src/advent_of_code/day20/main.py
@@ -1,70 +1,72 @@
 from typing import Generator, List, Tuple
 
-from advent_of_code.day16.main import COLUMN, ROW
 from advent_of_code.utils.file_utils import process_file
 from advent_of_code.day18.main import Point, dijkstra
 from advent_of_code.day12.main import get_manhattan_distance
 
 
-def get_track(file_name: str) -> Tuple[List[Point], Point, Point]:
+def get_track(file_name: str) -> Tuple[List[Point], List[Point], Point, Point]:
     lines: List[str] = process_file(
         file_name=file_name, process=lambda line: line.replace("\n", "")
     )
     points: List[Point] = list()
+    walls: List[Point] = list()
     start: Point
     end: Point
 
     for row in range(len(lines)):
         for column in range(len(lines[row])):
             if (value := lines[row][column]) == "#":
+                walls.append((row, column))
                 continue
 
             points.append((row, column))
             if value == "S":
+                walls.append((row, column))
                 start = (row, column)
             elif value == "E":
+                walls.append((row, column))
                 end = (row, column)
 
-    return points, start, end
+    return points, walls, start, end
 
 
 def get_cheats_for_track(
     original_track: List[Point],
-) -> Generator[Tuple[Point, Point, Point], None, None]:
-    def get_mid_point(p1: Point, p2: Point) -> Point:
-        return ((p1[ROW] + p2[ROW]) // 2, (p1[COLUMN] + p2[COLUMN]) // 2)
-
-    def get_end_cheat(
+) -> Generator[Tuple[Point, Point], None, None]:
+    def get_end_of_cheat(
         track: List[Point], target: Point
-    ) -> Generator[Tuple[Point, Point], None, None]:
+    ) -> Generator[Point, None, None]:
         for point in track:
             if get_manhattan_distance(target, point) != 2:
                 continue
-            if (wall := get_mid_point(target, point)) in track:
-                continue
             if point not in track:
                 continue
-            yield point, wall
-
+            yield point
         return None
 
-    for point in original_track:
-        for end_cheat, wall in get_end_cheat(track=original_track, target=point):
-            if (end_cheat, wall) is not None:
-                yield point, wall, end_cheat
+    for i in range(len(original_track)):
+        cheat_start = original_track[i]
+        for cheat_end in get_end_of_cheat(track=original_track[i:], target=cheat_start):
+            if cheat_end is not None:
+                yield cheat_start, cheat_end
 
 
 def solve_part_one(file_name: str, picoseconds_limit: int = 0) -> int:
-    points, start, end = get_track(file_name=file_name)
+    points, _, start, end = get_track(file_name=file_name)
     distances, path = dijkstra(maze=list(points), start=start, end=end)
     result = 0
 
-    for start_cheat, _, end_cheat in get_cheats_for_track(original_track=path):
-        saved_picoseconds = distances[end_cheat] - (distances[start_cheat] + 2)
+    for cheat_start, cheat_end in get_cheats_for_track(original_track=path):
+        saved_picoseconds = distances[cheat_end] - (distances[cheat_start] + 2)
         if saved_picoseconds != 0 and saved_picoseconds >= picoseconds_limit:
             result += 1
 
     return result
+
+
+def solve_part_two(file_name: str, picoseconds_limit: int = 0) -> int:
+    return 0
 
 
 def main() -> None:
--- tests/test_day20.py
+++ tests/test_day20.py
@@ -6,3 +6,8 @@ from advent_of_code.day20.main import solve_part_one
 @pytest.mark.parametrize("file_name, expected", [("input/day20/example.txt", 44)])
 def test_solve_part_one(file_name: str, expected: int) -> None:
     assert solve_part_one(file_name=file_name) == expected
+
+
+# @pytest.mark.parametrize("file_name, expected", [("input/day20/example.txt", 285)])
+# def test_solve_part_two(file_name: str, expected: int) -> None:
+#     assert solve_part_two(file_name=file_name) == expected
